f7a2df9d9c48dfea307da832e1112f449c621a87
Jason Mulligan <jason.mulligan@avoidwork.com>
2018-12-16 23:24:53 -0500

    Adding tests

--- test/eventsource.js
+++ test/eventsource.js
@@ -2,13 +2,28 @@ const path = require("path"),
 	eventsource = require(path.join("..", "index.js"));
 
 module.exports = {
+	heartbeat: {
+		setUp: function (done) {
+			this.eventsource = eventsource({ms: 1e4}, "Hello World!");
+			done();
+		},
+		test: function (test) {
+			test.expect(3);
+			test.equal(this.eventsource.heartbeat.ms, 1e4, `Should be '${1e4}'`);
+			test.equal(this.eventsource.initial.length, 1, "Should be '1'");
+			test.equal(this.eventsource.initial[0], "Hello World!", "Should be 'Hello World!'");
+			test.done();
+		}
+	},
 	listeners: {
 		setUp: function (done) {
 			this.eventsource = eventsource();
 			done();
 		},
 		test: function (test) {
-			test.expect(2);
+			test.expect(4);
+			test.equal(this.eventsource.heartbeat.ms, 0, "Should be '0'");
+			test.equal(this.eventsource.initial.length, 0, "Should be '0'");
 			test.equal(this.eventsource.listenerCount("data"), 0, "Should be '0'");
 			this.eventsource.on("data", () => console.log("Hello World!"));
 			test.equal(this.eventsource.listenerCount("data"), 1, "Should be '1'");
