4aa95f766325f874b44ecff60f00a62c7b5dee04
Jason Mulligan <jason.mulligan@avoidwork.com>
2018-12-16 23:27:12 -0500

    Adding tests

--- test/eventsource.js
+++ test/eventsource.js
@@ -17,16 +17,30 @@ module.exports = {
 	},
 	listeners: {
 		setUp: function (done) {
+			this.eventsource = eventsource("Hello World!");
+			done();
+		},
+		test: function (test) {
+			test.expect(5);
+			test.equal(this.eventsource.heartbeat.ms, 0, "Should be '0'");
+			test.equal(this.eventsource.initial.length, 1, "Should be '1'");
+			test.equal(this.eventsource.initial[0], "Hello World!", "Should be 'Hello World!'");
+			test.equal(this.eventsource.listenerCount("data"), 0, "Should be '0'");
+			this.eventsource.on("data", () => console.log("Hello World!"));
+			test.equal(this.eventsource.listenerCount("data"), 1, "Should be '1'");
+			test.done();
+		}
+	},
+	stock: {
+		setUp: function (done) {
 			this.eventsource = eventsource();
 			done();
 		},
 		test: function (test) {
-			test.expect(4);
+			test.expect(3);
 			test.equal(this.eventsource.heartbeat.ms, 0, "Should be '0'");
+			test.equal(this.eventsource.heartbeat.msg, "ping", "Should be 'ping'");
 			test.equal(this.eventsource.initial.length, 0, "Should be '0'");
-			test.equal(this.eventsource.listenerCount("data"), 0, "Should be '0'");
-			this.eventsource.on("data", () => console.log("Hello World!"));
-			test.equal(this.eventsource.listenerCount("data"), 1, "Should be '1'");
 			test.done();
 		}
 	}
