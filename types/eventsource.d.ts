f151549f30aa7671a60fe6d3eb0e98c055c9d90f
Jason Mulligan <jason.mulligan@avoidwork.com>
2023-10-30 15:23:18 -0400

    Updating `.d.ts` file

--- types/eventsource.d.ts
+++ types/eventsource.d.ts
@@ -2,13 +2,13 @@ export function eventsource(...args: any[]): EventSource;
 export class EventSource {
     constructor(config: any, ...args: any[]);
     heartbeat: {
-        event: any;
-        ms: any;
-        msg: any;
+        event: string;
+        ms: number;
+        msg: string;
     };
-    initial: any[];
+    initial: string[];
     init(req: any, res: any): this;
     listenerCount(eventName: string): number;
-    send(data: any, event: any, id: any): this;
+    send(data: any, event?: any, id?: any): this;
     setMaxListeners(arg: number): this;
 }
