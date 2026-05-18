ec1e3604f1c7f162f156a330e3f9ff6c0a272278
Jason Mulligan <jason.mulligan@avoidwork.com>
2026-05-18 18:28:14 -0400

    chore: improve TypeScript type definitions

--- types/eventsource.d.ts
+++ types/eventsource.d.ts
@@ -4,6 +4,9 @@ export interface EventSourceConfig {
     msg?: string;
 }
 
+type NodeHttp = import('node:http');
+type IncomingMessage = NodeHttp.IncomingMessage;
+type ServerResponse = NodeHttp.ServerResponse<IncomingMessage>;
 type EventSourceArg = string | EventSourceConfig;
 
 export function eventsource(...args: EventSourceArg[]): EventSource;
@@ -15,7 +18,7 @@ export declare class EventSource {
         msg: string;
     };
     initial: EventSourceArg[];
-    init(req: import('node:http').IncomingMessage | undefined, res: import('node:http').ServerResponse<import('node:http').IncomingMessage> | undefined): this;
+    init(req: IncomingMessage | undefined, res: ServerResponse | undefined): this;
     listenerCount(event: string): number;
     emit(event: string, arg?: unknown): boolean;
     send(data: unknown, event?: string, id?: number): this;
--- types/transmit.d.ts
+++ types/transmit.d.ts
@@ -1,6 +1,10 @@
+type NodeHttp = import('node:http');
+type IncomingMessage = NodeHttp.IncomingMessage;
+type ServerResponse = NodeHttp.ServerResponse<IncomingMessage>;
+
 export interface TransmitArg {
     data: string | Record<string, unknown>;
     event?: string;
 }
 
-export function transmit(res: import('node:http').ServerResponse<import('node:http').IncomingMessage>, arg?: TransmitArg, id?: number): void;
+export function transmit(res: ServerResponse, arg?: TransmitArg, id?: number): void;
